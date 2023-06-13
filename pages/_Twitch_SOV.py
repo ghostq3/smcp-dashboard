2232798db5c50688030f64792b6ce7461362e2c8
ghostq3 <103922258+ghostq3@users.noreply.github.com>
2023-06-14 01:44:32 +0800

    Update _Twitch_SOV.py

--- pages/_Twitch_SOV.py
+++ pages/_Twitch_SOV.py
@@ -15,7 +15,7 @@ def generate_line_chart(data, metrics1="", metrics2="", metrics3=""):
     """Generate Line charts."""
     chart_data = data.copy()
     chart_data['Date'] = pd.to_datetime(chart_data['Date'])  # Convert 'Date' column to datetime
-    chart_data.set_index(chart_data['Date'].dt.strftime('%b-%d'), inplace=True)  # Set 'Date' column as index in the month and day format
+    chart_data.set_index(chart_data['Date'].dt.strftime('%m-%d'), inplace=True)  # Set 'Date' column as index in the month and day format
 
     # Plot the line charts
     if metrics1 in chart_data.columns:
