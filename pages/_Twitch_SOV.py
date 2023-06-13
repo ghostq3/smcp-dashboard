70a8275265c9f746084ef519e368e4dea757c78c
ghostq3 <103922258+ghostq3@users.noreply.github.com>
2023-06-14 01:30:26 +0800

    Update _Twitch_SOV.py

--- pages/_Twitch_SOV.py
+++ pages/_Twitch_SOV.py
@@ -22,17 +22,18 @@ def generate_line_chart(data, metrics1="", metrics2="", metrics3=""):
 
     # Plot the line charts
     if metrics1 in chart_data.columns:
-        st.line_chart(chart_data[metrics1])
+        st.line_chart(chart_data[metrics1].T)  # Transpose the DataFrame
 
     if metrics2 in chart_data.columns:
-        st.line_chart(chart_data[metrics2])
+        st.line_chart(chart_data[metrics2].T)  # Transpose the DataFrame
 
     if metrics3 in chart_data.columns:
-        st.line_chart(chart_data[metrics3])
+        st.line_chart(chart_data[metrics3].T)  # Transpose the DataFrame
 
     # Add a message if no metrics were selected
     if not (metrics1 or metrics2 or metrics3):
         st.write("Please select at least one metric to display.")
+
 
 
 
